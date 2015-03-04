e3fa1f973c1d361746f6cc66917fc23d2801ccc3
gpsloureiro <gpsloureiro13@gmail.com>
2015-03-04 10:26:16 -0300

    Buscar Moto
    Ajustei na buscar da moto

--- PJ3_AV1_Gabriel_Loureiro/src/com/lojaveiculo/controller/BuscaMoto.java
+++ PJ3_AV1_Gabriel_Loureiro/src/com/lojaveiculo/controller/BuscaMoto.java
@@ -1,5 +1,6 @@
 package com.lojaveiculo.controller;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.lojaveiculo.loja.Loja;
@@ -11,213 +12,62 @@ Scanner input =  new Scanner(System.in);
 	
 	public void pesquisaMoto(Loja loja){
 		
-		String modelo = "", montadora = "", tipo = "";
+		String modelo = "", montadora = "", tipo = "", cor = "";
 		float preco = 0;
-		int verificador = 0;
 		
-		for (int i = 0; i < 10; i++) {
-			System.out.println();
-		}
-		
-		System.out.print("Informe o modelo da moto: ");
-		modelo = input.nextLine();
-		System.out.print("Informe a montadora da moto: ");
-		montadora = input.nextLine();
-		System.out.print("Informe o tipo da moto: ");
-		tipo = input.nextLine();
-		try {
-			System.out.print("Informe até quanto quer pagar no preço da moto: ");
-			preco = Float.parseFloat(input.nextLine());
-			if (preco < 0) {
-				System.out.println("Não existe esse preço então o valor vai ficar ZERO!!!");
-				preco = 0;
-			}
-		} catch (NumberFormatException e) {
-			preco = 0;
-		}
-		
-		if (montadora.equalsIgnoreCase("") && tipo.equalsIgnoreCase("") && preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (modelo.equalsIgnoreCase("") && tipo.equalsIgnoreCase("") && preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getMontadora().equalsIgnoreCase(montadora)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (montadora.equalsIgnoreCase("") && modelo.equalsIgnoreCase("") && preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getTipo().equalsIgnoreCase(tipo)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (montadora.equalsIgnoreCase("") && tipo.equalsIgnoreCase("") && modelo.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getPreco() < preco) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (tipo.equalsIgnoreCase("") && modelo.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getPreco() < preco && moto.getMontadora().equalsIgnoreCase(montadora)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (montadora.equalsIgnoreCase("") && tipo.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getPreco() < preco && moto.getModelo().equalsIgnoreCase(modelo)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (montadora.equalsIgnoreCase("") && modelo.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getPreco() < preco && moto.getTipo().equalsIgnoreCase(tipo)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (montadora.equalsIgnoreCase("") && preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo) && moto.getTipo().equalsIgnoreCase(tipo)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (modelo.equalsIgnoreCase("") && preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getTipo().equalsIgnoreCase(tipo)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (tipo.equalsIgnoreCase("") && preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo) && moto.getMontadora().equalsIgnoreCase(montadora)) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (montadora.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo) && moto.getTipo().equalsIgnoreCase(tipo)
-				&& moto.getPreco() < preco) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (tipo.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo) && moto.getMontadora().equalsIgnoreCase(montadora)
-				&& moto.getPreco() < preco) {
-					exibir(moto);
-					verificador = 1;
-				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
+		montadora = listarMontadora(loja);
+		modelo = listarModelo(loja, montadora);
 		if (modelo.equalsIgnoreCase("")) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getTipo().equalsIgnoreCase(tipo)
-				&& moto.getPreco() < preco) {
-					exibir(moto);
-					verificador = 1;
+			preco = precoCarro();
+			if (preco == 0) {
+				for (Moto moto : loja.getMotocicletas()) {
+					if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getModelo().equalsIgnoreCase(modelo)) {
+						exibir(moto);
+					}
+				}
+			}else {
+				for (Moto moto : loja.getMotocicletas()) {
+					if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getPreco() <= preco) {
+						exibir(moto);
+					}
 				}
 			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (preco == 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo) && moto.getTipo().equalsIgnoreCase(tipo)
-				&& moto.getMontadora().equalsIgnoreCase(montadora)) {
-					exibir(moto);
-					verificador = 1;
+		}else{
+			tipo = listarTipo(loja, montadora, modelo);
+			if (tipo.equalsIgnoreCase("")) {
+				for (Moto moto : loja.getMotocicletas()) {
+					if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getModelo().equalsIgnoreCase(modelo)) {
+						exibir(moto);
+					}
 				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
-			}
-		}
-		
-		if (!montadora.equalsIgnoreCase("") && !modelo.equalsIgnoreCase("") && !tipo.equalsIgnoreCase("") && preco != 0) {
-			for (Moto moto : loja.getMotocicletas()) {
-				if (moto.getModelo().equalsIgnoreCase(modelo) && moto.getTipo().equalsIgnoreCase(tipo)
-				&& moto.getPreco() < preco && moto.getMontadora().equalsIgnoreCase(montadora)) {
-					exibir(moto);
-					verificador = 1;
+			}else{
+				cor = listaCor(loja, montadora, modelo, tipo);
+				if (cor.equalsIgnoreCase("")) {
+					for (Moto moto : loja.getMotocicletas()) {
+						if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getModelo().equalsIgnoreCase(modelo)
+						&& moto.getTipo().equalsIgnoreCase(tipo)) {
+							exibir(moto);
+						}
+					}
+				}else{
+					preco = precoCarro();
+					if (preco == 0) {
+						for (Moto moto : loja.getMotocicletas()) {
+							if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getModelo().equalsIgnoreCase(modelo)
+							&& moto.getTipo().equalsIgnoreCase(tipo) && moto.getCor().equalsIgnoreCase(cor)) {
+								exibir(moto);
+							}
+						}
+					}else {
+						for (Moto moto : loja.getMotocicletas()) {
+							if (moto.getMontadora().equalsIgnoreCase(montadora) && moto.getModelo().equalsIgnoreCase(modelo)
+							&& moto.getTipo().equalsIgnoreCase(tipo) && moto.getCor().equalsIgnoreCase(cor) && 
+							moto.getPreco() <= preco) {
+								exibir(moto);
+							}
+						}
+					}
 				}
-			}
-			if (verificador == 0) {
-				System.out.println("Não tem nenhuma moto encontrada!!!!");
 			}
 		}
 		
@@ -235,5 +85,173 @@ Scanner input =  new Scanner(System.in);
 		System.out.println("Preço: " + moto.getPreco());
 		System.out.println("Capacidade: " + moto.getCapacidade());
 	}
+	
+    public String listarMontadora(Loja loja){
+		
+		ArrayList<String> escolhar = new ArrayList<String>();
+		
+		int contador = 0;
+		escolhar.add(loja.getMotocicletas().get(contador).getMontadora());			
+		for (int i = 0; i < loja.getMotocicletas().size(); i++) {
+			if (!escolhar.contains(loja.getMotocicletas().get(i).getMontadora())) {
+				escolhar.add(loja.getMotocicletas().get(i).getMontadora());
+			}
+		}
+		System.out.println();
+		System.out.println();
+		for (int i = 0; i < escolhar.size(); i++) {
+			System.out.println("   " + (i + 1) +" - " + escolhar.get(i));
+		}
+		System.out.println();
+		while (contador == 0) {
+			try {
+				System.out.print("Informe qual a Montadora do carro: ");
+				contador = Integer.parseInt(input.nextLine());
+				if (contador <= 0) {
+					System.out.println("Dever Escolher umas das opções!!!");
+					contador = 0;
+				}
+			} catch (NumberFormatException e) {
+				contador = 0;
+			}
+		}
+	    return escolhar.get(contador - 1);			
+		
+	}
+	
+    public String listarModelo(Loja loja, String montadora){
+		
+		ArrayList<String> escolhar = new ArrayList<String>();
+		
+		int contador = 1, opcao = 0;
+		for (int i = 0; i < loja.getMotocicletas().size(); i++) {
+			if (loja.getMotocicletas().get(i).getMontadora().equalsIgnoreCase(montadora)) {
+				if (!escolhar.contains(loja.getMotocicletas().get(i).getModelo())) {
+					escolhar.add(loja.getMotocicletas().get(i).getModelo());
+				}
+			}
+		}
+		System.out.println();
+		System.out.println();
+		for (int i = 0; i < escolhar.size(); i++) {
+			System.out.println("   " + (i + 1) +" - " + escolhar.get(i));
+			contador = contador + 1;
+		}
+		System.out.println("   " + contador +" - " + "Sair" );
+		System.out.println();
+		while (opcao == 0) {
+			try {
+				System.out.print("Informe qual o Modelo do carro: ");
+				opcao = Integer.parseInt(input.nextLine());
+				if (opcao <= 0 && opcao > contador) {
+					System.out.println("Dever Escolher umas das opções!!!");
+					opcao = 0;
+				}else if (opcao == contador) {
+					return "";
+				}
+			} catch (NumberFormatException e) {
+				opcao = 0;
+			}
+		}
+	    return escolhar.get(opcao - 1);	
+		
+	}
+    
+    public String listaCor(Loja loja, String montadora, String modelo, String tipo){
+		
+		ArrayList<String> escolhar = new ArrayList<String>();
+		
+		int contador = 1, opcao = 0;
+		for (int i = 0; i < loja.getMotocicletas().size(); i++) {
+			if (loja.getMotocicletas().get(i).getMontadora().equalsIgnoreCase(montadora) &&
+			loja.getMotocicletas().get(i).getModelo().equalsIgnoreCase(modelo) &&
+			loja.getMotocicletas().get(i).getTipo().equalsIgnoreCase(tipo)) {
+				if (!escolhar.contains(loja.getMotocicletas().get(i).getCor())) {
+					escolhar.add(loja.getMotocicletas().get(i).getCor());
+				}
+			}
+		}
+		System.out.println();
+		System.out.println();
+		for (int i = 0; i < escolhar.size(); i++) {
+			System.out.println("   " + (i + 1) +" - " + escolhar.get(i));
+			contador = contador + 1;
+		}
+		System.out.println("   " + contador +" - " + "Sair" );
+		System.out.println();
+		while (opcao == 0) {
+			try {
+				System.out.print("Informe qual a Cor do carro: ");
+				opcao = Integer.parseInt(input.nextLine());
+				if (opcao <= 0 && opcao > contador) {
+					System.out.println("Dever Escolher umas das opções!!!");
+					opcao = 0;
+				}else if (opcao == contador) {
+					return "";
+				}
+			} catch (NumberFormatException e) {
+				opcao = 0;
+			}
+		}
+	    return escolhar.get(opcao - 1);	
+		
+	}
+    
+    public String listarTipo(Loja loja, String montadora, String modelo){
+    	
+        ArrayList<String> escolhar = new ArrayList<String>();
+		
+		int contador = 1, opcao = 0;
+		for (int i = 0; i < loja.getMotocicletas().size(); i++) {
+			if (loja.getMotocicletas().get(i).getMontadora().equalsIgnoreCase(montadora) &&
+			loja.getMotocicletas().get(i).getModelo().equalsIgnoreCase(modelo)) {
+				if (!escolhar.contains(loja.getMotocicletas().get(i).getTipo())) {
+					escolhar.add(loja.getMotocicletas().get(i).getTipo());
+				}
+			}
+		}
+		System.out.println();
+		System.out.println();
+		for (int i = 0; i < escolhar.size(); i++) {
+			System.out.println("   " + (i + 1) +" - " + escolhar.get(i));
+			contador = contador + 1;
+		}
+    	while (opcao == 0) {
+			try {
+				System.out.print("Informe qual o Tipo do carro: ");
+				opcao = Integer.parseInt(input.nextLine());
+				if (opcao <= 0 && opcao > contador) {
+					System.out.println("Dever Escolher umas das opções!!!");
+					opcao = 0;
+				}else if (opcao == contador) {
+					return "";
+				}
+			} catch (NumberFormatException e) {
+				opcao = 0;
+			}
+		}
+    	return escolhar.get(opcao - 1);
+    }
+    
+    public float precoCarro(){
+    	
+    	float preco = -1;
+    	while (preco == -1) {
+			try {
+				System.out.print("Informe até quanto gostaria de pagar: ");
+				preco = Float.parseFloat(input.nextLine());
+				if (preco < 0 ) {
+					System.out.println("Dever informa uma valor valido!!!");
+					preco = -1;
+				}else if (preco == 0) {
+					return preco;
+				}
+			} catch (NumberFormatException e) {
+				preco = -1;
+			}
+		}
+    	return preco;
+    	
+    }
 
 }
